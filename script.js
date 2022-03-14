459c1cfeb8031ec2454ab77632f72464a61970b8
Miti <101258918+MitiBsq@users.noreply.github.com>
2022-03-14 13:48:55 +0200

    Stage 2 of the project

--- script.js
+++ script.js
@@ -4,19 +4,48 @@ const searchBox = document.getElementById('searchBox');
 const searchButton = document.getElementById('searchButton');
 const wordListPlace = document.getElementById('wordListPlace');
 const wordList = document.getElementById('wordList');
-let words = new Array();
-searchButton.addEventListener('click', submitSearch);
+wordListPlace.style.visibility = 'hidden';
 searchBox.focus();
-totalWords.innerHTML="Total number of Words: ";
-totalWords.innerHTML += words.length;
+totalWords.innerHTML = "Total number of Words: ";
+const verify = new Array();
+for (let i = 0; i < localStorage.length; i++) {
+    verify.push(localStorage.key(i));
+    let words = document.createElement('li');
+    words.innerText = localStorage.key(i);
+    wordList.appendChild(words);
+}
+// Adding an event to the searchButton + fixing the error of not typing something
+searchButton.addEventListener('click', () => {
+    if (searchBox.value !== "") {
+        if (verify.indexOf(searchBox.value) !== -1) {
+            document.getElementById('searchText').innerText = "Great! Word found";
+            document.getElementById('searchText').style.color = 'green';
+            showWord(searchBox.value);
+        } else {
+            nextPage();
+        }
+    } else {
+        document.getElementById('searchText').innerText = "Insert a word!";
+        document.getElementById('searchText').style.color = 'red';
+    }
 
-console.log(localStorage.getItem('wordCount'))
-function submitSearch() {
+});
+//VEzi ca cuvantul ramane salvat 
+const wordFound = document.createElement('h4');
+function showWord(element) {
+    wordFound.innerText = searchBox.value;
+    searchBoxLabel.appendChild(wordFound);
+}
+
+function nextPage() {
+    wordFound.remove();
+    wordListPlace.style.visibility = 'visible';
     //Moving the search box + search button on the right side
     searchBoxLabel.id = "newSearchLabel";
     document.getElementById('searchText').remove();
     searchBox.className = "form-control-sm";
     searchButton.className = 'btn btn-primary btn-sm';
+    searchBoxLabel.appendChild(document.createElement("h6")).innerText = "Sorry, we couldn't find your choosed word, maybe you can find it in the list below";
     //Creating a new add panel for adding new words
     const addWordButton = document.createElement('button');
     addWordButton.innerText = "Add";
@@ -27,16 +56,24 @@ function submitSearch() {
     addWordBox.placeholder = "Add a new word";
     addWordBox.className = "form-control";
     document.getElementById('addLabel').appendChild(addWordBox);
+    const addWordBoxDetail = document.createElement('input');
+    addWordBoxDetail.type = "text";
+    addWordBoxDetail.placeholder = "What it means?";
+    addWordBoxDetail.className = "form-control";
+    document.getElementById('addLabel').appendChild(addWordBoxDetail);
     // sa folosesc localStorage pentru stocarea datelor
-    /* addWordButton.addEventListener('click', () => {
-       let wordCount=localStorage.getItem(wordCount, 1);
-       console.log(wordCount)
-       let wordCountPlus=parseInt(wordCount,10);
-       wordCountPlus +=1;
-       console.log(wordCountPlus);
-       wordCount=wordCountPlus.toString(10);
-       console.log(wordCount)
-       localStorage.setItem
-    }); */
+    addWordButton.addEventListener('click', () => {
+        let newWord = document.createElement('li');
+        newWord.innerText = addWordBox.value;
+        let newWordDetail = addWordBoxDetail.value;
+        wordList.appendChild(newWord);
+        localStorage.setItem(addWordBox.value, "cuvant");
+        if (addWordBoxDetail.value === '') {
+            console.log('merge')
+        } else {
+            console.log(addWordBoxDetail.value)
+        }
+
+    });
 }
 
